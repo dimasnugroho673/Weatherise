4fca971b8ea3e673b359c7781d5c225aaf3b2276
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2022-05-31 17:21:03 +0700

    Initial Commit

---
+++ Weatherise SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weatherise SwiftUI
+//
+//  Created by Dimas Putro on 31/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weatherise SwiftUI/Weatherise_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weatherise_SwiftUIApp.swift
+//  Weatherise SwiftUI
+//
+//  Created by Dimas Putro on 31/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Weatherise_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
